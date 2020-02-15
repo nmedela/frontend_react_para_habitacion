b54a3a8db260bc945f36c7ea01764791e05ddcdd
nmedela <medela.nicolas@gmail.com>
2020-02-15 19:46:24 -0300

    subo el build al repo

---
+++ build/precache-manifest.b9942fb64035540c480b324378f4c5d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4dbf5a7cf78c813685c17a1d288e92f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6878fd87718b94735060",
+    "url": "/static/css/main.e5a0d122.chunk.css"
+  },
+  {
+    "revision": "3e600dc7cc88487f360f",
+    "url": "/static/js/2.d37fd73d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.d37fd73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6878fd87718b94735060",
+    "url": "/static/js/main.9666c526.chunk.js"
+  },
+  {
+    "revision": "7dda5372f5cc2dd454c5",
+    "url": "/static/js/runtime-main.9b374270.js"
+  }
+]);
