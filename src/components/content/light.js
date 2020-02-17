b02bd3e0b4f1c21f4e93cf104c050bb67ce19e33
nmedela <medela.nicolas@gmail.com>
2020-02-16 23:44:18 -0300

    Se agrega menu para las luces

--- src/components/content/light.js
+++ src/components/content/light.js
@@ -1,21 +1,52 @@
 import React from 'react'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
+import DenseAppBar from './../menu/appBar'
 
-class Light extends React.Component{
-render(){
+class Light extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: false,
+            isReady: true,
+            light: {
 
-    return(
-        <div>
-                <Grid container spacing={3}>
-                    <Grid  item xs={12} align='center'>
-                        <CircularProgress disableShrink style={{ width: '100px', height: '100px' }} />
-                        <p>En construccion...</p>
+            }
+        }
+    }
+    render() {
+        if (!this.state.isReady) {
+            return (
+                <div>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} align='center'>
+                            <CircularProgress disableShrink style={{ width: '100px', height: '100px' }} />
+                            <p>En construccion...</p>
+                        </Grid>
                     </Grid>
-                </Grid>
-        </div>
-    )
+                </div>)
+        }
+        if (this.state.isLoading) {
+            return (
+                <div>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} align='center'>
+                            <CircularProgress disableShrink style={{ width: '100px', height: '100px' }} />
+                            <p>Cargando...</p>
+                        </Grid>
+                    </Grid>
+                </div>)
+        }
 
-}
+        return (
+            <div>
+                <DenseAppBar />
+                <div style={{backgroundColor:'rgb(200,200,200)', height:'320px'}}>
+                    container
+                </div>
+            </div>
+        )
+
+    }
 }
 export default Light
