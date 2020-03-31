8329a10882c8f0a81a34994dd3cf04330f4de003
nmedela <medela.nicolas@gmail.com>
2020-03-31 20:44:51 -0300

    Hago el build desde aca

--- build/precache-manifest.5c3aca3d5531a49d7d0538d92051685f.js
+++ build/precache-manifest.5c3aca3d5531a49d7d0538d92051685f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9eb7e8fd3b85e4cb76a6a4256b27a891",
+    "revision": "925115e0bb24294084f3a9f602fe7467",
     "url": "/index.html"
   },
   {
-    "revision": "5cb42761e55e1da9c1c9",
+    "revision": "95405feb3af490c44f80",
     "url": "/static/css/main.e5a0d122.chunk.css"
   },
   {
-    "revision": "01da719dd213dab56f84",
-    "url": "/static/js/2.b34de612.chunk.js"
+    "revision": "91ed300d0902a4a40d9a",
+    "url": "/static/js/2.6b609da2.chunk.js"
   },
   {
     "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
-    "url": "/static/js/2.b34de612.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.6b609da2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5cb42761e55e1da9c1c9",
-    "url": "/static/js/main.8607de39.chunk.js"
+    "revision": "95405feb3af490c44f80",
+    "url": "/static/js/main.46d075bd.chunk.js"
   },
   {
     "revision": "7dda5372f5cc2dd454c5",
